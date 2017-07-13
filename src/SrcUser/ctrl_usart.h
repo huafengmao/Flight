40f67b38d9e36f93bbd2f01162efabfcfab5e2cd
Shuaipeng Wang <Antiver@foxmail.com>
2017-07-13 11:17:41 +0800

    UART

--- src/SrcUser/ctrl_usart.h
+++ src/SrcUser/ctrl_usart.h
@@ -11,7 +11,7 @@
 
 #include "ctrl_basic.h"
 #include "string.h"
-#include "usart.h"
+//#include "usart.h"
 
 #define USART_TX_BUF_LEN    300             // 串口发送缓存区空间大小
 #define USART_RX_BUF_LEN    300             // 串口接收缓存区空间大小
@@ -42,11 +42,11 @@ extern YesNo_t U3_Rx_End;                   // 串口 3 是否接收完毕指定
 extern YesNo_t U3_Tx_End;                   // 串口 3 是否发送完毕指定数据
 
 extern void Usart_Rx_Buf_Init(void);                                                // 串口接收缓存区内容初始化
-extern void Usart_Change_Baud(UART_HandleTypeDef* pHuart, uint32 NewBaud);          // 修改串口通信波特率
-extern void Usart_Send_Byte(UART_HandleTypeDef* huart, uint8 Byte);                 // 指定串口发送一个字节数据
-extern void Usart_Send_Data(UART_HandleTypeDef* huart, uint8* pTx, uint8 len);      // 指定的串口 发送指定内容数据
-extern void Usart_Send_String(UART_HandleTypeDef* huart, const char* pTx);          // 指定的串口发送字符串数据
+//extern void Usart_Change_Baud(UART_HandleTypeDef* pHuart, uint32 NewBaud);          // 修改串口通信波特率
+//extern void Usart_Send_Byte(UART_HandleTypeDef* huart, uint8 Byte);                 // 指定串口发送一个字节数据
+//extern void Usart_Send_Data(UART_HandleTypeDef* huart, uint8* pTx, uint8 len);      // 指定的串口 发送指定内容数据
+//extern void Usart_Send_String(UART_HandleTypeDef* huart, const char* pTx);          // 指定的串口发送字符串数据
 //extern void Usart_Receive_IT(UART_HandleTypeDef *huart, uint8 *pRx, uint16_t len);  // 指定的串口接收指定长度的数据，存放在指定的空间
-extern void Usart_Send_TxBuf_Filled_Data(UART_HandleTypeDef *huart, uint16_t len);  // 指定的串口发送缓存区已经填充过内容 按照指定的长度发送数据
+//extern void Usart_Send_TxBuf_Filled_Data(UART_HandleTypeDef *huart, uint16_t len);  // 指定的串口发送缓存区已经填充过内容 按照指定的长度发送数据
 
 #endif  // __CTRL_USART_H__
